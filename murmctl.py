f02ab0b5f576bb212f09c2e4434f47cd0d03f9c7
drakearonhalt <drakearonhalt@gmail.com>
2014-01-12 23:57:00 +0000

    Added list options. Output not formatted.

--- murmctl.py
+++ murmctl.py
@@ -3,6 +3,9 @@
 import Ice
 Ice.loadSlice('-I/usr/share/Ice-3.5.1/slice /opt/murmur/Murmur.ice')
 import Murmur
+import argparse
+
+
 
 props = Ice.createProperties()
 props.setProperty('Ice.Default.EncodingVersion','1.0')
@@ -18,7 +21,28 @@ meta = Murmur.MetaPrx.checkedCast(proxy)
 
 server = meta.getServer(1)
 
-channels = server.getChannels()
+parser = argparse.ArgumentParser()
+parser.add_argument('command', help='command to perform on murmur server')
+parser.add_argument('object', help='object the command should be performed on',
+        default='None')
+args = parser.parse_args()
+
+
+if args.command == 'list':
+    if args.object == 'channels':
+        print server.getChannels()
+    elif args.object == 'users':
+        print server.getUsers()
+    elif args.object == 'bans':
+        print server.getBans()
+    elif args.object == 'registered':
+        print server.getRegisteredUsers('')
+    elif args.object == 'tree':
+        print server.getTree()
+
+
+
+
 
 
 
